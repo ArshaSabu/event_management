8f3d016feb2c202bae6dc97057155a95a45efc28
ashnakunju <ashnakunju2003@gmail.com>
2025-10-31 23:33:34 +0530

    Home added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <Header />
-      <main style={{ paddingTop: "100px", minHeight: "70vh" }}>
+      {/* <main style={{ paddingTop: "100px", minHeight: "70vh" }}>
         <h2 style={{ textAlign: "center", marginTop: 60 }}>
           Page body / hero area
         </h2>
-      </main>
+      </main> */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
       <Footer />
     </>
   );
